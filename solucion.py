94d019ffdc403c2ce42ddfecb9556b294c3177d3
devdeploy <af_mauro7@hotmail.com>
2025-04-30 20:47:32 -0300

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -5,7 +5,7 @@ def sumar_ascii(cadena):
     Ejemplo:
     "hola mundo" -> h(104) + o(111) + l(108) + a(97) + (32) + m(109) + u(117) + n(110) + d(100) + o(111) = 999
     """
-    # Los estudiantes deben implementar esta función
+    #  implementar esta función
     suma = 0
     for caracter in cadena:
         suma += ord(caracter) + ord(caracter)
